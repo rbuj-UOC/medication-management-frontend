4dd34c2241307c6f659a573b75f11025ceacba8e
rbuj <robert.buj@gmail.com>
2025-04-26 10:02:09 +0200

    schedule: drop hour, minute and disabled fields

--- src/app/Schedule/models/schedule.dto.ts
+++ src/app/Schedule/models/schedule.dto.ts
@@ -2,14 +2,11 @@ export class ScheduleDTO {
   id!: number;
   medication_id!: number;
   start_date: Date;
-  end_date: Date;
+  end_date!: Date;
   frequency!: string;
   cron_expression!: string;
-  disabled!: boolean;
   created_at!: Date;
   updated_at!: Date;
-  hour: number;
-  minute: number;
 
   constructor(start_date: Date) {
     this.start_date = start_date;
