34d0528773ad56f425a1e537f58a78686bbbed79
rbuj <robert.buj@gmail.com>
2025-05-02 10:36:06 +0200

    populate prifile form using subscribe

--- src/app/User/components/profile/profile.component.ts
+++ src/app/User/components/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { formatDate } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
@@ -9,7 +9,7 @@ import {
   Validators
 } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { selectUserRole } from '../../../Auth/selectors';
+import { Observable } from 'rxjs';
 import * as UserAction from '../../actions';
 import { UserDTO } from '../../models/user.dto';
 import { selectUser } from '../../selectors';
@@ -20,8 +20,8 @@ import { selectUser } from '../../selectors';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
-  profileUser: UserDTO;
+export class ProfileComponent implements OnInit {
+  selectUser$: Observable<UserDTO | null>;
 
   name: FormControl;
   surname_1: FormControl;
@@ -35,51 +35,48 @@ export class ProfileComponent {
 
   profileForm: FormGroup;
   isValidForm: boolean | null;
-  isAdmin: boolean | null;
   roles: { value: string; label: string }[];
 
   constructor(
     private formBuilder: FormBuilder,
     private store: Store
   ) {
-    this.profileUser = new UserDTO('', '', '', '', new Date(), '', '');
-
     this.isValidForm = null;
 
-    this.name = new FormControl(this.profileUser.name, [
+    this.name = new FormControl('', [
       Validators.required,
       Validators.minLength(5),
       Validators.maxLength(25)
     ]);
 
-    this.surname_1 = new FormControl(this.profileUser.surname_1, [
+    this.surname_1 = new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(25)
     ]);
 
-    this.surname_2 = new FormControl(this.profileUser.surname_2, [
+    this.surname_2 = new FormControl('', [
       Validators.minLength(3),
       Validators.maxLength(25)
     ]);
 
-    this.alias = new FormControl(this.profileUser.alias, [
+    this.alias = new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(25)
     ]);
 
     this.birth_date = new FormControl(
-      formatDate(this.profileUser.birth_date, 'yyyy-MM-dd', 'en'),
+      formatDate(new Date(), 'yyyy-MM-dd', 'en'),
       [Validators.required]
     );
 
-    this.email = new FormControl(this.profileUser.email, [
+    this.email = new FormControl('', [
       Validators.required,
       Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')
     ]);
 
-    this.password = new FormControl(this.profileUser.password, [
+    this.password = new FormControl('', [
       Validators.required,
       Validators.minLength(8)
     ]);
@@ -89,42 +86,12 @@ export class ProfileComponent {
       this.validateSamePassword
     ]);
 
-    this.role = new FormControl(this.profileUser.role, [Validators.required]);
+    this.role = new FormControl('', [Validators.required]);
     this.role.disable();
     this.roles = [
       { value: 'admin', label: 'Admin' },
       { value: 'user', label: 'User' }
     ];
-
-    this.profileForm = this.formBuilder.group({
-      name: this.name,
-      surname_1: this.surname_1,
-      surname_2: this.surname_2,
-      alias: this.alias,
-      birth_date: this.birth_date,
-      email: this.email,
-      password: this.password
-    });
-
-    this.store.select(selectUserRole).subscribe((user_role) => {
-      if (user_role) {
-        this.isAdmin = user_role === 'admin';
-      }
-    });
-
-    this.store.select(selectUser).subscribe((user) => {
-      this.profileUser = user;
-
-      this.name.setValue(this.profileUser.name);
-      this.surname_1.setValue(this.profileUser.surname_1);
-      this.surname_2.setValue(this.profileUser.surname_2);
-      this.alias.setValue(this.profileUser.alias);
-      this.birth_date.setValue(
-        formatDate(this.profileUser.birth_date, 'yyyy-MM-dd', 'en')
-      );
-      this.email.setValue(this.profileUser.email);
-      this.role.setValue(this.profileUser.role);
-    });
 
     this.profileForm = this.formBuilder.group({
       name: this.name,
@@ -139,13 +106,30 @@ export class ProfileComponent {
     });
   }
 
+  ngOnInit(): void {
+    this.selectUser$ = this.store.select(selectUser);
+    this.selectUser$.subscribe((user) => {
+      if (user) {
+        this.profileForm.patchValue({
+          name: user.name,
+          surname_1: user.surname_1,
+          surname_2: user.surname_2,
+          alias: user.alias,
+          birth_date: formatDate(user.birth_date, 'yyyy-MM-dd', 'en'),
+          email: user.email,
+          role: user.role
+        });
+      }
+    });
+    this.store.dispatch(UserAction.getUser());
+  }
+
   onSubmit(): void {
     this.isValidForm = false;
     if (this.profileForm.invalid) {
       return;
     }
     this.isValidForm = true;
-    this.profileUser = this.profileForm.value;
     const { confirmPassword, ...user } = this.profileForm.value;
     this.store.dispatch(UserAction.updateUser({ user }));
   }
