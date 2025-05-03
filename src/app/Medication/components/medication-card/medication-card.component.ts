067ed563bcbbac1c4db0604cb1bf09008d6fe86d
rbuj <robert.buj@gmail.com>
2025-05-03 13:45:56 +0200

    madication grid+card components

--- src/app/Medication/components/medication-card/medication-card.component.ts
+++ src/app/Medication/components/medication-card/medication-card.component.ts
@@ -2,27 +2,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MedicationDTO } from '../../models/medication.dto';
 
 @Component({
-  selector: 'app-medication-table',
+  selector: 'app-medication-card',
   // eslint-disable-next-line @angular-eslint/prefer-standalone
   standalone: false,
-  templateUrl: './medication-table.component.html',
-  styleUrls: ['./medication-table.component.scss']
+  templateUrl: './medication-card.component.html',
+  styleUrls: ['./medication-card.component.scss']
 })
-export class MedicationTableComponent {
-  @Input() medications: MedicationDTO[];
-  @Output() createMedicationRequest = new EventEmitter<void>();
+export class MedicationCardComponent {
+  @Input() medication: MedicationDTO;
   @Output() deleteMedicationRequest = new EventEmitter<number>();
   @Output() editMedicationRequest = new EventEmitter<number>();
-
-  displayedColumns: string[] = [
-    'medication-name',
-    'medication-schedules',
-    'medication-actions'
-  ];
-
-  createMedication(): void {
-    this.createMedicationRequest.emit();
-  }
 
   deleteMedication(id: number, name: string): void {
     const result = confirm('Confirm delete medication: ' + name);
--- src/app/Medication/components/medication-grid/medication-grid.component.ts
+++ src/app/Medication/components/medication-grid/medication-grid.component.ts
@@ -2,33 +2,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MedicationDTO } from '../../models/medication.dto';
 
 @Component({
-  selector: 'app-medication-table',
+  selector: 'app-medication-grid',
   // eslint-disable-next-line @angular-eslint/prefer-standalone
   standalone: false,
-  templateUrl: './medication-table.component.html',
-  styleUrls: ['./medication-table.component.scss']
+  templateUrl: './medication-grid.component.html',
+  styleUrls: ['./medication-grid.component.scss']
 })
-export class MedicationTableComponent {
+export class MedicationGridComponent {
   @Input() medications: MedicationDTO[];
   @Output() createMedicationRequest = new EventEmitter<void>();
   @Output() deleteMedicationRequest = new EventEmitter<number>();
   @Output() editMedicationRequest = new EventEmitter<number>();
 
-  displayedColumns: string[] = [
-    'medication-name',
-    'medication-schedules',
-    'medication-actions'
-  ];
-
   createMedication(): void {
     this.createMedicationRequest.emit();
   }
 
-  deleteMedication(id: number, name: string): void {
-    const result = confirm('Confirm delete medication: ' + name);
-    if (result) {
-      this.deleteMedicationRequest.emit(id);
-    }
+  deleteMedication(id: number): void {
+    this.deleteMedicationRequest.emit(id);
   }
 
   editMedication(id: number): void {
