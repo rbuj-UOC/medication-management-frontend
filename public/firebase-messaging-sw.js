500c3bd7a5c2810cba4f6bdc0c939479ccc4f7ad
rbuj <robert.buj@gmail.com>
2025-05-24 18:30:52 +0200

    drop firebase-messaging-sw.js.sample

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -9,12 +9,12 @@ importScripts(
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
+  apiKey: "AIzaSyBTG8e0-Zh3YwbuFPKgoDraL1jO39fBUJM",
+  authDomain: "medication-management-ap-506be.firebaseapp.com",
+  projectId: "medication-management-ap-506be",
+  storageBucket: "medication-management-ap-506be.firebasestorage.app",
+  messagingSenderId: "486357535988",
+  appId: "1:486357535988:web:7196e39d5430ab5450e2a1"
 };
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
